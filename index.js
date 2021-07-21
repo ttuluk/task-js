a93ce162e53004db129800930d9f34a7549d2692
ttuluk <ttasya.k@gmail.com>
2021-07-21 22:10:26 +0300

    add index

---
+++ index.js
@@ -0,0 +1,9 @@
+// import './module-1/t1.js';
+// import './module-1/t2.js';
+import './module-1/t3.js';
+// import './module-1/t4.js';
+// import './module-1/t5.js';
+// import './module-1/t6.js';
+// import './module-1/t7.js';
+// import './module-1/t8.js';
+// import './module-1/t9.js';
